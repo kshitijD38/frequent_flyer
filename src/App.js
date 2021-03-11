3022626a0265175a794e42f00594ac8c90adae38
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-03-11 23:28:31 +0530

    . commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // fetch(
+  //   "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/%7Bcountry%7D/GBP/en-GB/?query=Stockholm",
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "x-rapidapi-key": "9da154b716msh63b249836851661p12258cjsn71a386811e08",
+  //       "x-rapidapi-host":
+  //         "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+  //     },
+  //   }
+  // )
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
+  const options = {
+    method: "GET",
+    url:
+      "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/",
+    params: { query: "Stockholm" },
+    headers: {
+      "x-rapidapi-key": "9da154b716msh63b249836851661p12258cjsn71a386811e08",
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  return <div className="App"></div>;
 }
 
 export default App;
